d225f51e4de7407b93b706f44c9b33f581e81897
Williamoverman <overmanwilliam17@gmail.com>
2023-06-24 16:13:18 +0200

    27 fix 9

--- commands/lol-registration/register-lol-account.js
+++ commands/lol-registration/register-lol-account.js
@@ -36,7 +36,7 @@ module.exports = {
     connection.connect(err => {
       if (err) {
         console.error(err);
-        return interaction.editReply({ content: 'Something went wrong with the database connnection :(', embeds: [], components: []});
+        interaction.editReply({ content: 'Something went wrong with the database connnection :(', embeds: [], components: []});
       }
       console.log('Connected to database!');
     
@@ -59,70 +59,73 @@ module.exports = {
             embeds: [lolEmbed],
             components: [row],
           });
-
-          const collectorFilter = i => i.user.id === interaction.user.id;
-
-          try {
-            const confirmation = response.awaitMessageComponent({ filter: collectorFilter, time: 180_000 });
-      
-            if (confirmation.customId === 'ready') {
-            confirmation.update({ content: `...`, components: [] });
-            var leagueUsername = interaction.options.getString('username');
-  
-            const apiLink = `https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${leagueUsername}?api_key=${process.env.LOLAPITOKEN}`
-        
-            fetch(apiLink)
-            .then(response => {
-              if (!response.ok) {
-                throw new Error('API request failed');
-              }
-              return response.json();
-            })
-            .then(data => {
-              if (data?.status?.status_code === 404) {
-                connection.end();
-                console.log("Connection closed.");
-                return interaction.editReply({ content: 'No summonerer found.', embeds: [], components: []});
-              } else {
-                const profileIconId = data.profileIconId;
-                leagueUsername = data.name;
-                if (profileIconId == 1) {
-                  const userData = { discordID: discordUserID, usernameLoL: leagueUsername };
-                  const insertUserQuery = 'INSERT INTO LoLregistration SET ?'
-                  connection.query(insertUserQuery, userData, (err, result) => {
-                    if (err) {
-                      console.error('Error inserting data:', err);
-                      interaction.editReply({ content: 'Something went wrong with registering :(', embeds: [], components: []});
-                      connection.end();
-                      console.log("Connection closed.");
-                    } else {
-                      console.log('Data inserted successfully!');
-                      interaction.editReply({ content: 'Thank you for registering! :)', embeds: [], components: []});
-                      connection.end();
-                      console.log("Connection closed.");
-                    }
-                  });
-                } else {
-                  interaction.editReply({ content: 'Incorrect profile picture.', embeds: [], components: []});
-                  connection.end();
-                  console.log("Connection closed.");
-                }
-              }
-            })
-            .catch(error => {
-              console.error(error);
-              interaction.editReply({ content: 'Something went wrong with the API.', embeds: [], components: []});
-              connection.end();
-              console.log("Connection closed.");
-            });
-            } 
-          } catch (e) {
-            connection.end();
-            console.log("Connection closed.");
-            return interaction.editReply({ content: 'Confirmation not received within 3 minutes, cancelling...', embeds: [], components: []});
-          }
         }
       });  
     })
+
+    const collectorFilter = i => i.user.id === interaction.user.id;
+
+      try {
+        const confirmation = await response.awaitMessageComponent({ filter: collectorFilter, time: 180_000 });
+  
+        if (confirmation.customId === 'ready') {
+          await confirmation.update({ content: `...`, components: [] });
+        } 
+      } catch (e) {
+        await interaction.editReply({ content: 'Deleting message..', embeds: [], components: []});
+        setTimeout(() => {
+          return interaction.deleteReply();
+        }, 5000);
+      }
+  
+      var leagueUsername = interaction.options.getString('username');
+  
+      const apiLink = `https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${leagueUsername}?api_key=${process.env.LOLAPITOKEN}`
+  
+      fetch(apiLink)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('API request failed');
+        }
+        return response.json();
+      })
+      .then(data => {
+        if (data.status.status_code = 404) {
+          interaction.editReply({ content: 'No summonerer found.', embeds: [], components: []});
+          connection.end();
+          console.log("Connection closed.");
+          return;
+        } else {
+          const profileIconId = data.profileIconId;
+          leagueUsername = data.name;
+          if (profileIconId == 1) {
+            const userData = { discordID: discordUserID, usernameLoL: leagueUsername };
+            const insertUserQuery = 'INSERT INTO LoLregistration SET ?'
+            connection.query(insertUserQuery, userData, (err, result) => {
+              if (err) {
+                console.error('Error inserting data:', err);
+                interaction.editReply({ content: 'Something went wrong with registering :(', embeds: [], components: []});
+                connection.end();
+                console.log("Connection closed.");
+              } else {
+                console.log('Data inserted successfully!');
+                interaction.editReply({ content: 'Thank you for registering! :)', embeds: [], components: []});
+                connection.end();
+                console.log("Connection closed.");
+              }
+            });
+          } else {
+            interaction.editReply({ content: 'Incorrect profile picture.', embeds: [], components: []});
+            connection.end();
+            console.log("Connection closed.");
+          }
+        }
+      })
+      .catch(error => {
+        console.error(error);
+        interaction.editReply({ content: 'Something went wrong with the API.', embeds: [], components: []});
+        connection.end();
+        console.log("Connection closed.");
+      });
   },
 };
