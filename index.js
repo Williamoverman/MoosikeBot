7255e65ca87088015ea6c19b11422e295f4868cb
Williamoverman <overmanwilliam17@gmail.com>
2023-06-25 19:01:46 +0200

    delete message after cd up

--- index.js
+++ index.js
@@ -84,13 +84,14 @@ client.on(Events.InteractionCreate, async interaction => {
 
 	if (timestamps.has(interaction.user.id)) {
 		const expirationTime = timestamps.get(interaction.user.id) + cooldownAmount;
+		const remainingTime = Math.ceil((expirationTime - now) / 1000);
 
 		if (now < expirationTime) {
 			const expiredTimestamp = Math.round(expirationTime / 1000);
 			interaction.reply({ content: `Please wait, you are on a cooldown for \`${command.data.name}\`. You can use it again <t:${expiredTimestamp}:R>.`, ephemeral: true });
 			setTimeout(() => {
 				return interaction.deleteReply();
-			}, expiredTimestamp);
+			}, remainingTime * 1000);
 		}
 	}
 
