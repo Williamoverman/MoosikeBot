bab83bd27e25696dbae0fbe5895f3cb47fa43503
Williamoverman <overmanwilliam17@gmail.com>
2023-06-25 14:56:29 +0200

    checking guilds

--- commands/fun/cross-chat.js
+++ commands/fun/cross-chat.js
@@ -2,8 +2,6 @@ const { Client, GatewayIntentBits, SlashCommandBuilder } = require('discord.js')
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMembers,
-        GatewayIntentBits.GuildMessages
     ]
 });
 
@@ -15,31 +13,11 @@ module.exports = {
             option.setName('message')
                 .setDescription('The message you want to send')),
     async execute(interaction) {
-        const Guilds = client.guilds.cache.map(guild => guild.id);
-        console.log(Guilds);
-        const messageContent = interaction.options.getString('message');
+        const guilds = client.guilds.cache;
 
-        // Get the target server's ID
-        const targetServerId = interaction.guild.id;
-
-        // Check if the target server is cached
-        const targetServer = client.guilds.cache.get(targetServerId);
-        if (!targetServer) {
-            console.log(`Target server (${targetServerId}) is not cached.`);
-            console.log('Cached guilds:', client.guilds.cache);
-            await interaction.reply('Invalid target server ID.');
-            return;
-        }
-
-        // Find the "cross-server-chat" channel in the target server
-        const targetChannel = targetServer.channels.cache.find(channel => channel.name === 'cross-server-chat' && channel.type === 'GUILD_TEXT');
-        if (!targetChannel) {
-            await interaction.reply('No "cross-server-chat" channel found in the target server.');
-            return;
-        }
-
-        // Send the message to the target channel
-        await targetChannel.send(messageContent);
-        await interaction.reply('Message sent successfully!');
+        guilds.forEach((guild) => {
+            console.log(`Guild Name: ${guild.name}`);
+            console.log(`Guild ID: ${guild.id}`);
+        });
     },
 };
