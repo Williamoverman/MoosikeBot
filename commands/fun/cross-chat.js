a15544c7fdb523b7a21a9b76aadfc16f869a8e26
Williamoverman <overmanwilliam17@gmail.com>
2023-06-25 14:37:08 +0200

    added string option

--- commands/fun/cross-chat.js
+++ commands/fun/cross-chat.js
@@ -4,19 +4,24 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('connect')
-		.setDescription('Connect to a specified server!'),
+		.setDescription('Connect to a specified server!')
+        .addStringOption(option =>
+            option.setName('message')
+                .setDescription('The message you want to send')),
 	async execute(interaction) {
-        const guilds = client.guilds.cache;
-        const sendingGuild = message.guild;
-        const sendingChannel = message.channel;
+		const guilds = client.guilds.cache;
+		const sendingGuild = interaction.guild;
+		const sendingChannel = interaction.channel;
 
-        guilds.forEach((guild) => {
-            if (guild !== sendingGuild) {
-                const targetChannel = guild.channels.cache.find((channel) => channel.name === 'cross-server-chat');
-                if (targetChannel) {
-                targetChannel.send(`[Cross-Server Chat] ${message.author.username}: ${chatMessage}`);
-                }
-            }
-        });
+		const chatMessage = interaction.options.getString('message'); // Assuming you have a string option named 'message'
+
+		guilds.forEach((guild) => {
+			if (guild !== sendingGuild) {
+				const targetChannel = guild.channels.cache.find((channel) => channel.name === 'cross-server-chat');
+				if (targetChannel) {
+					targetChannel.send(`[Cross-Server Chat] ${interaction.user.username}: ${chatMessage}`);
+				}
+			}
+		});
 	},
 };
