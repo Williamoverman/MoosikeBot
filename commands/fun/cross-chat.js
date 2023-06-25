7bb6512bc1109baaa91d6163a07b189b4882a2f8
Williamoverman <overmanwilliam17@gmail.com>
2023-06-25 14:48:39 +0200

    small fix 1

--- commands/fun/cross-chat.js
+++ commands/fun/cross-chat.js
@@ -1,37 +1,43 @@
 const { Client, GatewayIntentBits, SlashCommandBuilder } = require('discord.js');
-const client = new Client({ 
+const client = new Client({
     intents: [
-        GatewayIntentBits.Guilds, 
+        GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMembers,
         GatewayIntentBits.GuildMessages
-    ] 
+    ]
 });
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('connect')
-		.setDescription('Connect to a specified server!')
+    data: new SlashCommandBuilder()
+        .setName('connect')
+        .setDescription('Connect to a specified server!')
         .addStringOption(option =>
             option.setName('message')
                 .setDescription('The message you want to send')),
-	async execute(interaction) {
+    async execute(interaction) {
         const messageContent = interaction.options.getString('message');
 
-        const targetServerId = 1121419451775078581;
+        // Get the target server's ID
+        const targetServerId = interaction.guild.id;
 
+        // Check if the target server is cached
         const targetServer = client.guilds.cache.get(targetServerId);
         if (!targetServer) {
+            console.log(`Target server (${targetServerId}) is not cached.`);
+            console.log('Cached guilds:', client.guilds.cache);
             await interaction.reply('Invalid target server ID.');
             return;
         }
 
+        // Find the "cross-server-chat" channel in the target server
         const targetChannel = targetServer.channels.cache.find(channel => channel.name === 'cross-server-chat' && channel.type === 'GUILD_TEXT');
         if (!targetChannel) {
             await interaction.reply('No "cross-server-chat" channel found in the target server.');
             return;
         }
 
+        // Send the message to the target channel
         await targetChannel.send(messageContent);
         await interaction.reply('Message sent successfully!');
-	},
+    },
 };
