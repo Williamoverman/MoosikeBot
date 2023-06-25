a45a2e6760a9cb69d73bc97ba74a95dec51bcfb3
Williamoverman <overmanwilliam17@gmail.com>
2023-06-25 14:34:47 +0200

    small fix

--- commands/fun/cross-chat.js
+++ commands/fun/cross-chat.js
@@ -1,4 +1,5 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { Client, GatewayIntentBits, SlashCommandBuilder } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 module.exports = {
 	data: new SlashCommandBuilder()
