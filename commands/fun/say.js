a99d519ff9e0edfea9553e74987a560d4d258017
Williamoverman <overmanwilliam17@gmail.com>
2023-06-26 21:41:32 +0200

    test

--- commands/fun/say.js
+++ commands/fun/say.js
@@ -9,6 +9,22 @@ module.exports = {
               .setDescription('Whatever you want the bot to say!')),
 	async execute(interaction) {
         let userInteraction = interaction.options.getString('userinput') ?? 'No input given.';
+		const logEmbed = new EmbedBuilder()
+		.setColor(0x0099FF)
+		.setTitle(`The executed command name: ${interaction.commandName}`)
+		.setAuthor({ name: interaction.user.username, iconURL: interaction.user.avatarURL() })
+		.setTimestamp()
+		
+		const channelName = 'logs';
+	
+		const guild = interaction.guild;
+		const channel = guild.channels.cache.find(ch => ch.name === channelName);
+	
+		if (!channel) {
+		  console.log(`Channel "${channelName}" not found.`);
+		}
+	
+		channel.send(logEmbed);
 		await interaction.reply(userInteraction);
 	},
 };
