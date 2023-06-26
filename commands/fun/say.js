06b6cf7269f23dfe9f90a2813c5c95677d931c58
Williamoverman <overmanwilliam17@gmail.com>
2023-06-26 21:20:37 +0200

    test

--- commands/fun/say.js
+++ commands/fun/say.js
@@ -8,6 +8,16 @@ module.exports = {
             option.setName('userinput')
               .setDescription('Whatever you want the bot to say!')),
 	async execute(interaction) {
+		const channelName = 'logs';
+  
+      const guild = interaction.guild;
+      const channel = guild.channels.cache.find(ch => ch.name === channelName);
+  
+      if (!channel) {
+        console.log(`Channel "${channelName}" not found.`);
+      }
+  
+      channel.send('wtf');
         let userInteraction = interaction.options.getString('userinput') ?? 'No input given.';
 		await interaction.reply(userInteraction);
 	},
