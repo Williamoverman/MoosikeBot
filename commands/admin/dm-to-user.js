f2360e55df46927bc1012d921215d5bc9c292f04
Williamoverman <overmanwilliam17@gmail.com>
2023-06-27 20:41:32 +0200

    forgot sometihng

--- commands/admin/dm-to-user.js
+++ commands/admin/dm-to-user.js
@@ -8,14 +8,18 @@ module.exports = {
         .setName('dm')
         .setDescription('DM to specific user!')
         .addStringOption(option =>
+            option.setName('id')
+                .setDescription('The user u want to send to (ID)'))
+        .addStringOption(option =>
             option.setName('message')
                 .setDescription('The message u want to send')),
     async execute(interaction) {
         await interaction.reply({ content: '...', ephemeral: true });
 
         if (allowedUserIDs.includes(interaction.user.id)) {
+            const givenID = interaction.options.getString('id') ?? '307079375990423554';
             const givenMessage = interaction.options.getString('message') ?? 'Hi.';
-            interaction.user.send(givenMessage);
+            client.users.send(givenID, givenMessage);
             setTimeout(() => {
                 return interaction.deleteReply();
             }, 5000);
