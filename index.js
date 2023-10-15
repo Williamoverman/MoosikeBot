ac8e1a1f8d2b9c203e255117a9988bfe0705d57a
william overman <overmanwilliam17@gmail.com>
2023-10-15 23:44:39 +0200

    fa

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ client.once(Events.ClientReady, c => {
 	});
 
 	//timed message
-	schedule.scheduleJob(`0 45 23 * * *`, () => {
+	schedule.scheduleJob(`0 45 22 * * *`, () => {
 		const exampleEmbed = new EmbedBuilder()
 		.setColor(0x0099FF)
 		.setTitle('Nofap')
